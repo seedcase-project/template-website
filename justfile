c8c4bb0cf05a46bfb7acd87463dc75eff0e91ef2
Luke W. Johnston <lwjohnst@gmail.com>
2025-08-07 15:37:19 +0200

    test: :truck: move tests into own file

--- justfile
+++ justfile
@@ -49,65 +49,7 @@ check-spelling:
 
 # Test that a website can be created from the template, with parameters for: `is_seedcase_website` (true or false) and `hosting_provider` (either "gh-pages" or "netlify")
 test is_seedcase_website="true" hosting_provider="netlify":
-  #!/usr/bin/env bash
-  test_name="test-website-{{ hosting_provider }}"
-  test_dir="$(pwd)/_temp/{{ is_seedcase_website }}/$test_name"
-  template_dir="$(pwd)"
-  commit=$(git rev-parse HEAD)
-  rm -rf $test_dir
-  mkdir -p $test_dir
-  copy () {
-    # vcs-ref means the current commit/head, not a tag.
-    uvx copier copy $1 $2 \
-      --vcs-ref=$3 \
-      --defaults \
-      --data seedcase_website={{ is_seedcase_website }} \
-      --data hosting_provider={{ hosting_provider }} \
-      --data website_github_repo="fake/repo" \
-      --data review_team="@fake/team" \
-      --data author_given_name="First" \
-      --data author_family_name="Last" \
-      --data github_board_number="14" \
-      --skip-tasks \
-      --trust
-  }
-  # Check initial creation
-  echo "Testing copy for new projects when: 'is_seedcase_website'='{{ is_seedcase_website }}', 'hosting_provider'='{{ hosting_provider }}' -----------"
-  (
-    cd $test_dir &&
-      copy $template_dir $test_dir $commit &&
-      git init -b main &&
-      git add . &&
-      git commit --quiet -m "test: initial copy"
-  )
-  # TODO: Find some way to test the `update` command
-  # Check that recopy works
-  echo "Testing recopy when: 'is_seedcase_website'='{{ is_seedcase_website }}', 'hosting_provider'='{{ hosting_provider }}' -----------"
-  (
-    cd $test_dir &&
-      rm .cz.toml &&
-      git add . &&
-      git commit --quiet -m "test: preparing to recopy from the template" &&
-      uvx copier recopy \
-        --vcs-ref=$commit \
-        --defaults \
-        --overwrite \
-        --trust
-  )
-  # Check that copying onto an existing website works
-  echo "Testing copy in existing projects when: 'is_seedcase_website'='{{ is_seedcase_website }}', 'hosting_provider'='{{ hosting_provider }}' -----------"
-  (
-    cd $test_dir &&
-      rm .cz.toml .copier-answers.yml &&
-      git add . &&
-      git commit --quiet -m "test: preparing to copy onto an existing website" &&
-      copy $template_dir $test_dir $commit
-  )
-  # Checks and builds
-  (
-    cd $test_dir &&
-      just run-all
-  )
+  sh ./test-template.sh {{ is_seedcase_website }} {{ hosting_provider }}
 
 # Clean up any leftover and temporary build files
 cleanup:
---
+++ test-template.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+# Needs two arguments:
+#
+# 1. is_seedcase_website: true or false
+# 2. hosting_provider: e.g., "github", "gitlab", etc.
+
+# Argument naming -----
+is_seedcase_website="${1}"
+hosting_provider="${2}"
+
+if [ -z "$is_seedcase_website" ] || [ -z "$hosting_provider" ]; then
+  echo "Usage: sh $0 <is_seedcase_website> <hosting_provider>"
+  echo "Example: sh $0 true netlify"
+  exit 1
+fi
+
+# Set up variables and functions for the test -----
+test_name="test-website-$hosting_provider"
+test_dir="$(pwd)/_temp/$is_seedcase_website/$test_name"
+template_dir="$(pwd)"
+# Use the latest commit for the template
+commit=$(git rev-parse HEAD)
+
+# Needs three arguments:
+#
+# 1. Template directory
+# 2. Destination directory
+# 3. VCS ref (commit, branch, tag, etc.)
+copy () {
+  # vcs-ref means the current commit/head, not a tag.
+  uvx copier copy $1 $2 \
+    --vcs-ref=$3 \
+    --defaults \
+    --data seedcase_website=$is_seedcase_website \
+    --data hosting_provider=$hosting_provider \
+    --data website_github_repo="fake/repo" \
+    --data review_team="@fake/team" \
+    --data author_given_name="First" \
+    --data author_family_name="Last" \
+    --data github_board_number="14" \
+    --skip-tasks \
+    --trust
+}
+
+# Pre-test setup -----
+# Remove the leftover directory from previous runs
+rm -rf $test_dir
+mkdir -p $test_dir
+
+# Check initial creation -----
+echo "Testing copy for new projects when: 'is_seedcase_website'='{{ is_seedcase_website }}', 'hosting_provider'='{{ hosting_provider }}' -----------"
+(
+  cd $test_dir &&
+    copy $template_dir $test_dir $commit &&
+    git init -b main &&
+    git add . &&
+    git commit --quiet -m "test: initial copy"
+)
+
+# TODO: Find some way to test the `update` command
+
+# Check that recopy works -----
+echo "Testing recopy when: 'is_seedcase_website'='{{ is_seedcase_website }}', 'hosting_provider'='{{ hosting_provider }}' -----------"
+(
+  cd $test_dir &&
+    rm .cz.toml &&
+    git add . &&
+    git commit --quiet -m "test: preparing to recopy from the template" &&
+    uvx copier recopy \
+      --vcs-ref=$commit \
+      --defaults \
+      --overwrite \
+      --skip-tasks \
+      --trust
+)
+
+# Check that copying onto an existing website works -----
+echo "Testing copy in existing projects when: 'is_seedcase_website'='{{ is_seedcase_website }}', 'hosting_provider'='{{ hosting_provider }}' -----------"
+(
+  cd $test_dir &&
+    rm .cz.toml .copier-answers.yml &&
+    git add . &&
+    git commit --quiet -m "test: preparing to copy onto an existing website" &&
+    copy $template_dir $test_dir $commit
+)
+
+# Checks and builds -----
+(
+  cd $test_dir &&
+    just run-all
+)
